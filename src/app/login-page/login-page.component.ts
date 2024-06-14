17a9e2c3d342acdd96c114734c4c6defd9b8a2d3
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2024-06-14 19:21:07 +1000

    Feat: add on click functions in login page

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,9 +1,11 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -14,12 +16,31 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
+    CommonModule,
+    FormsModule,
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-  @Output()
+  @Output() onSubmitLoginEvent = new EventEmitter();
+  @Output() onSubmitRegisterEvent = new EventEmitter();
+
+  hide = signal(true);
+
   username: string = '';
   password: string = '';
+
+  onSubmitLogin(): void {
+    console.log('hi ' + this.username);
+  }
+
+  onSubmitRegister(): void {
+    console.log('bye ' + this.username);
+  }
+
+  showPassEvent(event: MouseEvent) {
+    this.hide.set(!this.hide);
+    event.stopPropagation();
+  }
 }
